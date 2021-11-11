850d63f85baeef837076eb266dcdd2078daa0064
Vladimir Monakhov <monvla16@gmail.com>
2021-11-11 10:04:43 +0300

    power fibo prime half of medium

--- 03_power_fibonacci_prime/fibonacci/fibo.py
+++ 03_power_fibonacci_prime/fibonacci/fibo.py
@@ -3,30 +3,64 @@ from console_tester import *
 import math
 
 
+class SquareMatrix:
+    """
+    | item00, item01 |
+    | item10, item11 |
+    """
+
+    def __init__(self, item00, item01, item10, item11):
+        self.item00 = item00
+        self.item01 = item01
+        self.item10 = item10
+        self.item11 = item11
+
+    def multiply(self, second_matrix):
+        return SquareMatrix(
+            self.item00 * second_matrix.item00 + self.item01 * second_matrix.item10,
+            self.item00 * second_matrix.item01 + self.item01 * second_matrix.item11,
+            self.item10 * second_matrix.item00 + self.item11 * second_matrix.item10,
+            self.item10 * second_matrix.item01 + self.item11 * second_matrix.item11
+        )
+
+    def print(self):
+        print(
+"""
+|%s, %s|
+|%s, %s|
+""" % (self.item00, self.item01, self.item10, self.item11)
+        )
+
+
 class FiboAlgo(Task):
 
     def __init__(self):
         self.n = -1
         self.count = 0
+        self.Fi = (1 + math.sqrt(5)) / 2
 
     def run(self, data):
         needed_fibo_value = int(data[0])
         # result = self.solve_with_recursion(needed_fibo_value)
-        result = self.solve_with_iterations(needed_fibo_value)
+        # result = self.solve_with_iterations(needed_fibo_value)
+        # result = self.solve_golden(needed_fibo_value)
+        result = self.solve_matrix(needed_fibo_value)
         return self.prepare_result(result)
 
-    def fibo(self, n):
+    def fibo_rec(self, n):
         if n == 0:
             return 0
         if n == 1:
             return 1
-        return self.fibo(n - 1) + self.fibo(n - 2)
+        return self.fibo_rec(n - 1) + self.fibo_rec(n - 2)
 
     def solve_with_recursion(self, needed_fibo_value):
-        result = self.fibo(needed_fibo_value)
+        """ Рекурсивный алгоритм """
+        result = self.fibo_rec(needed_fibo_value)
         return result
 
     def solve_with_iterations(self, needed_fibo_value):
+        """ Поиск через итерации """
         fibo_array = [0, 1]
         if needed_fibo_value < 2:
             return fibo_array[needed_fibo_value]
@@ -35,6 +69,47 @@ class FiboAlgo(Task):
             fibo_array.pop(0)
         result = fibo_array.pop()
         return result
+
+    def solve_golden(self, needed_fibo_value):
+        """ Формула золотого сечения """
+        return int(pow(self.Fi, needed_fibo_value) / math.sqrt(5) + 0.5)
+
+    def to_power_binary(self, needed_fibo_value):
+        """ Возведение в степень через двоичное разложение показателя степени """
+        result = SquareMatrix(
+            1, 1,
+            1, 0
+        )
+        n = needed_fibo_value
+        numbers = []
+        current_power = 1
+        number_in_power = result
+        while n >= 1:
+            if n % 2 == 1:
+                result = result.multiply(number_in_power)
+            if n > 1:
+                current_power *= 2
+                number_in_power = number_in_power.multiply(number_in_power)
+                n = int(n / 2)
+                numbers.append(n)
+            else:
+                break
+        return result
+
+    def solve_matrix(self, needed_fibo_value):
+        """ Решение через домножение матриц """
+        start = SquareMatrix(
+            1, 1,
+            1, 0
+        )
+        if needed_fibo_value == 0:
+            return start.item11
+        if needed_fibo_value == 1:
+            return start.item10
+        if needed_fibo_value == 2:
+            return start.item01
+        result = self.to_power_binary(needed_fibo_value)
+        return result.item11
 
 
 task = FiboAlgo()
--- 03_power_fibonacci_prime/power/power.py
+++ 03_power_fibonacci_prime/power/power.py
@@ -1,6 +1,5 @@
 import pathlib
 from console_tester import *
-import math
 
 
 class PowerAlgo(Task):
@@ -10,28 +9,53 @@ class PowerAlgo(Task):
         self.count = 0
 
     def run(self, data):
-        result = self.solve_with_iterations(data)
+        power = int(data[1])
+        multiplier = float(data[0])
+        # result = self.solve_with_iterations(power, multiplier)
+        result = self.solve_binary(power, multiplier)
+        # result = self.solve_with_multiplying(power, multiplier)
         return self.prepare_result(round(result, 11))
 
-    def solve_with_iterations(self, data):
+    def solve_with_iterations(self, power, multiplier):
+        """ Итеративный алгоритм возведения числа в степень """
         result = 1.0
-        for _ in range(int(data[1])):
-            result *= float(data[0])
+        for _ in range(power):
+            result *= multiplier
         return result
 
-    # def solve_with_power(self, data):
-    #     result = float(data[0])
-    #     needed_power = int(data[1])
-    #     if needed_power == 0:
-    #         return 1.0
-    #
-    #     current_power = 1
-    #     while current_power * 2 < needed_power:
-    #         current_power *= 2
-    #         result *= result
-    #     for _ in range(needed_power - current_power):
-    #         result *= float(data[0])
-    #     return result
+    def solve_with_multiplying(self, power, multiplier):
+        """ Возведение числа в степень через домножение """
+        result = multiplier
+        needed_power = power
+        if needed_power == 0:
+            return 1.0
+
+        current_power = 1
+        while current_power * 2 < needed_power:
+            current_power *= 2
+            result *= result
+        for _ in range(needed_power - current_power):
+            result *= multiplier
+        return result
+
+    def solve_binary(self, power, multiplier):
+        """ Возведение в степень через двоичное разложение показателя степени """
+        result = 1.0
+        n = power
+        numbers = []
+        current_power = 1
+        number_in_power = multiplier
+        while n >= 1:
+            if n % 2 == 1:
+                result *= number_in_power
+            if n > 1:
+                current_power *= 2
+                number_in_power *= number_in_power
+                n = int(n / 2)
+                numbers.append(n)
+            else:
+                break
+        return result
 
 task = PowerAlgo()
 tester = Tester(
